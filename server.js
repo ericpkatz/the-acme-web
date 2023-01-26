5957dd2e6c9d5c3e2b78c0e954e478d3600fcec3
ericpkatz <ericpkatz@gmail.com>
2023-01-25 21:34:32 -0500

    moved to express

--- server.js
+++ server.js
@@ -1,97 +1,70 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res)=> {
-  if(req.url === '/'){
-    res.write(`
-      <html>
-        <head>
-          <title>The Acme Web</title>
-          <style>
-            nav a.selected {
-              border: solid 1px dodgerBlue;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>the Acme Web</h1>
-          <nav>
-            <a href='/' class='selected'>Home</a>
-            <a href='/about'>About Us</a>
-            <a href='/products'>Products</a>
-          </nav>
-        </body>
-      </html>
-    `);
-    res.end();
-  }
-  else if(req.url === '/about'){
-    res.write(`
-      <html>
-        <head>
-          <title>About Us</title>
-          <style>
-            nav a.selected {
-              border: solid 1px dodgerBlue;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>the Acme Web</h1>
-          <nav>
-            <a href='/'>Home</a>
-            <a href='/about' class='selected'>About Us</a>
-            <a href='/products'>Products</a>
-          </nav>
-          <div>The Acme Web is one of the greatest sites ever made</div>
-        </body>
-      </html>
-    `);
-    res.end();
-  }
-  else if(req.url === '/products'){
-    res.write(`
-      <html>
-        <head>
-          <title>Products</title>
-          <style>
-            nav a.selected {
-              border: solid 1px dodgerBlue;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>the Acme Web</h1>
-          <nav>
-            <a href='/'>Home</a>
-            <a href='/about'>About Us</a>
-            <a class='selected' href='/products'>Products</a>
-          </nav>
-          <ul>
-            <li>Foo</li>
-            <li>Bar</li>
-            <li>Bazz</li>
-          </ul>
-        </body>
-      </html>
-    `);
-    res.end();
-  }
-  else {
-    res.statusCode = 404;
-    res.write(`
-      <html>
-        <head>
-          <title>The Acme Web</title>
-        </head>
-        <body>
-          <h1>the Acme Web</h1>
-          <div>${req.url} was not found!!</div>
-        </body>
-      </html>
-    `);
-    res.end();
-  }
+const server = express();
+
+server.use(express.static('static'));
+server.use((req, res, next)=> {
+  console.log(req.url);
+  next();
 });
+
+server.get('/', (req, res)=> {
+  res.send(`
+    <html>
+      <head>
+        <title>Acme Web</title>
+        <link rel='stylesheet' href='/styles.css' />
+      </head>
+      <body>
+        <h1>The Acme Web</h1>
+        <nav>
+          <a href='/'>Home</a>
+          <a href='/about'>About</a>
+          <a href='/products'>Products</a>
+        </nav>
+      </body>
+    </html>
+  `);
+});
+
+server.get('/about', (req, res)=> {
+  res.send(`
+    <html>
+      <head>
+        <title>Acme About</title>
+        <link rel='stylesheet' href='/styles.css' />
+      </head>
+      <body>
+        <h1>The Acme Web About</h1>
+        <nav>
+          <a href='/'>Home</a>
+          <a href='/about'>About</a>
+          <a href='/products'>Products</a>
+        </nav>
+      </body>
+    </html>
+  `);
+});
+
+server.get('/products', (req, res)=> {
+  res.send(`
+    <html>
+      <head>
+        <title>Acme Products</title>
+        <link rel='stylesheet' href='/styles.css' />
+      </head>
+      <body>
+        <h1>The Acme Web Products</h1>
+        <nav>
+          <a href='/'>Home</a>
+          <a href='/about'>About</a>
+          <a href='/products'>Products</a>
+        </nav>
+      </body>
+    </html>
+  `);
+});
+
 
 const port = process.env.PORT || 3000;
 
